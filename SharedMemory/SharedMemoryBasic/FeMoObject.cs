e0bcfecfbdb94c0b663d0ef11c7f0ad493c3dc1d
Momo <egg14829@spengergasse.at>
2013-12-13 11:52:23 +0100

    Momo merge

--- SharedMemory/SharedMemoryBasic/FeMoObject.cs
+++ SharedMemory/SharedMemoryBasic/FeMoObject.cs
@@ -1,111 +1,101 @@
 using System;
 using System.Collections.Generic;
 
-namespace SharedMemory
-{
-	public class FeMoObject : Dictionary<String, String> {
+namespace SharedMemory {
+  public class FeMoObject : Dictionary<String, String> {
+    private string name;
+    private float version = 0.1f;
+    private long id;
 
-		private string name;
-		private float version = 0.1f;
-		private long id;
+    public Int64 ID {
+      set {
+        id = value;
+      }
+      get {
+        return id;
+      }
+    }
 
-		public Int64 ID {
-			set {
-				id = value;
-			}
-			get {
-				return id;
-			}
-		}
+    public String Name {
+      set {
+        name = value;
+      }
+      get {
+        return name;
+      }
+    }
 
-		public String Name {
-			set {
-				name = value;
-			}
-			get {
-				return name;
-			}
-		}
+    public new void Add(String key, String value) {
+      String key_ = getTypePrefix(value) + key;
+      String value_ = optimizeValue(key, value);
+      base.Add(key_, value_);
+    }
 
-		public new void Add (String key, String value)
-		{
-			String key_ = getTypePrefix(value) + key;
-			String value_ = optimizeValue(key, value);
-			base.Add(key_, value_);
-		}
+    public void AddInt(String key, Int64 value) {
+      Add(key, value + "");
+    }
 
-		public void AddInt (String key, Int64 value)
-		{
-			Add (key, value + "");
-		}
+    public void AddFloat(String key, float value) {
+      Add(key, value + "");
+    }
 
-		public void AddFloat (String key, float value)
-		{
-			Add (key, value + "");
-		}
+    public void AddDouble(String key, double value) {
+      Add(key, value + "");
+    }
 
-		public void AddDouble (String key, double value)
-		{
-			Add (key, value + "");
-		}
+    public void AddObject(String key, FeMoObject value) {
+      Add(key, value.ToString());
+    }
 
-		public void AddObject (String key, FeMoObject value)
-		{
-			Add (key, value.ToString());
-		}
+    public void AddString(String key, String value) {
+      Add(key, value);
+    }
 
-		public void AddString (String key, String value)
-		{
-			Add (key, value);
-		}
+    private String optimizeValue(String key, String value) {
+      String key_ = getTypePrefix(value);
+      if (key_.StartsWith("o_")) {
+        String[] p = value.Split("[".ToCharArray());
+        String[] parts = p[2].Split("||".ToCharArray());
+        String[] p_ = parts[4].Split("=".ToCharArray());
+        String id = p_[1].Split("]".ToCharArray())[0];
+        Int64 i_;
+        if (!Int64.TryParse(id, out i_))
+          throw new MemoryWrongIdException("Parse Error");
+        Int64 id_ = Int64.Parse(id);
+        if (VirtualMemory.InMemory(id_))
+          return "fmo_" + id_;
+        else
+          return "mem_err";
+      }
+      return value;
+    }
 
-		private String optimizeValue (String key, String value)
-		{
-			String key_ = getTypePrefix (value);
-			if (key_.StartsWith("o_")) {
-				String[] p = value.Split("[".ToCharArray());
-				String[] parts = p[2].Split("||".ToCharArray());
-				String[] p_ = parts[4].Split("=".ToCharArray());
-				String id = p_[1].Split("]".ToCharArray())[0];
-				Int64 i_;
-				if(!Int64.TryParse(id, out i_))
-					throw new MemoryWrongIdException("Parse Error");
-				Int64 id_ = Int64.Parse(id);
-				if(VirtualMemory.InMemory(id_))
-					return "fmo_" + id_;
-				else
-					return "mem_err";
-			}
-			return value;
-		}
+    private String getTypePrefix(String value) {
+      float f = 0.0f;
+      double d = 0.0d;
+      Int64 i = 0;
+      if (Int64.TryParse(value, out i))
+        return "i_";
+      if (float.TryParse(value, out f))
+        return "f_";
+      if (Double.TryParse(value, out d))
+        return "d_";
+      if (value.Contains("fmo:=["))
+        return "o_";
+      return "s_";
+    }
 
-		private String getTypePrefix(String value) {
-			float f = 0.0f;
-			double d = 0.0d;
-			Int64 i = 0;
-			if(Int64.TryParse(value, out i))
-				return "i_";
-			if(float.TryParse(value, out f))
-				return "f_";
-			if(Double.TryParse(value, out d))
-				return "d_";
-			if(value.Contains("fmo:=["))
-				return "o_";
-			return "s_";
-		}
-
-		public override string ToString ()
-		{
-			string head =  "fmo:=[head:=[name:=" + name + "||vers:=" + version + "||id=" + id + "]";
-			head += "||body:=[";
-			foreach(String key in Keys) {
-				head += key + ":=" + this[key] + "||";
-			}
-			if(Keys.Count > 0)
-				head = head.Substring(0, head.Length - 2);
-			head += "]]";
-			return head;
-		}
-	}
+    public override string ToString() {
+      string head = "fmo:=[head:=[name:=" + name + "||vers:=" + version + "||id=" + id + "]";
+      head += "||body:=[";
+      foreach (String key in Keys) {
+        head += key + ":=" + this[key] + "||";
+      }
+      if (Keys.Count > 0)
+        head = head.Substring(0, head.Length - 2);
+      head += "]]";
+      return head;
+    }
+  }
 }
 
--- SharedMemory/SharedMemoryBasic/FeMoUtilities.cs
+++ SharedMemory/SharedMemoryBasic/FeMoUtilities.cs
@@ -1,51 +1,67 @@
 using System;
 using System.Collections.Generic;
+using System.Xml.Linq;
 
 namespace SharedMemory {
-  public class FeMoReader {
-    public static List<FeMoObject> ReadObject(String femo) {
-      String[] lines = femo.Split("\n");
-      List<FeMoObject> list = new List<FeMoObject>();
-      foreach (String str in lines) {
-        int i = str.IndexOf("body:=[");
-        String head = str.Substring(0, i - 3);
-        String body = str.Substring(i + 7);
+	public class FeMoUtilities {
+		public static List<FeMoObject> ReadObject(String femo) {
+			String[] lines = femo.Split("\n");
+			List<FeMoObject> list = new List<FeMoObject>();
+			foreach (String str in lines) {
+				int i = str.IndexOf("body:=[");
+				String head = str.Substring(0, i - 3);
+				String body = str.Substring(i + 7);
 
-        //Head Splitting
-        i = head.IndexOf("head:=");
-        head = head.Substring(i);
-        i = head.IndexOf("name:=");
-        head = head.Substring(i);
+				//Head Splitting
+				i = head.IndexOf("head:=");
+				head = head.Substring(i);
+				i = head.IndexOf("name:=");
+				head = head.Substring(i);
 
 
-        FeMoObject feMoOb = new FeMoObject();
+				FeMoObject feMoOb = new FeMoObject();
 
-        String[] headS = head.Split("||");
-        foreach (String s in headS) {
-          switch (s) {
-            case s.StartsWith("name"):
-              feMoOb.Name = s.Substring(s.IndexOf(":=") + 2);
-              break;
-            case s.StartsWith("vers"):
-              feMoOb.Version = s.Substring(s.IndexOf(":=") + 2);
-              break;
-            case s.StartsWith("id"):
-              feMoOb.ID = s.Substring(s.IndexOf(":=") + 2);
-              break;
-          }
-        }
+				String[] headS = head.Split("||");
+				foreach (String s in headS) {
+					switch (s) {
+						case s.StartsWith("name"):
+							feMoOb.Name = s.Substring(s.IndexOf(":=") + 2);
+							break;
+						case s.StartsWith("vers"):
+							feMoOb.Version = s.Substring(s.IndexOf(":=") + 2);
+							break;
+						case s.StartsWith("id"):
+							feMoOb.ID = s.Substring(s.IndexOf(":=") + 2);
+							break;
+					}
+				}
 
-        //Body splitting
-        body = head.Substring(i);
+				//Body splitting
+				body = head.Substring(i);
 
-        String[] bodyS = head.Split("||");
-        foreach (String s in bodyS) {
-          String[] forsplit = s.Split(":=");
-          feMoOb.Add(forsplit[0], forsplit[1]);
-        }
-        list.Add(feMoOb);
-      }
-      return list;
-    }
-  }
+				String[] bodyS = head.Split("||");
+				foreach (String s in bodyS) {
+					String[] forsplit = s.Split(":=");
+					feMoOb.Add(forsplit[0], forsplit[1]);
+				}
+				list.Add(feMoOb);
+			}
+			return list;
+		}
+
+		public XElement XMLDumper(List<FeMoObject> femo) {
+			XElement xe = new XElement("FeMoObjects",
+				              from a in femo
+				          select
+				              new XElement("FeMo",
+					              new XAttribute("Name", a.Name),
+					              new XAttribute("Version", a.Version),
+					              new XAttribute("ID", a.ID),
+					              new XElement("Body",
+						              from b in a
+						              select new XElement("Object",
+							                  new XElement("Key", b.Key),
+							                  new XElement("Value", b.Value)))));
+		}
+	}
 }
