7991f39a82fe48537a9cd742c72b4e802457642e
Momo <egg14829@spengergasse.at>
2014-01-21 16:03:32 +0100

    Revert "Commit 27.3"
    This reverts commit 090b36b92cd6e569d5ab719dc3bc654d5d92c3f8.

---
+++ SharedMemory/SharedMemoryBasic/FeMoObjectTest.cs
@@ -0,0 +1,102 @@
+using System;
+using nunit.framework;
+using SharedMemory;
+
+namespace SharedMemory
+{
+	[TestFixture ()]
+	public class FeMoObjectTest
+	{
+		[Test ()]
+		public void TestCase ()
+		{
+			Assert.Throws<MemoryNotInitializedException> (VirtualMemory.Dump);
+			Assert.Throws<MemoryAllocationError> (throw_1);
+			Assert.Throws<MemoryAllocationError> (throw_2);
+			Assert.Throws<MemoryAllocationError> (throw_3);
+			Assert.DoesNotThrow (throw_4);
+			VirtualMemory.Initialize (100000);
+			VirtualMemory.Dump ();
+			Assert.Throws<MemoryNotInitializedException>(VirtualMemory.Dump);
+			Assert.Throws<MemoryAllocationError>(throw_1);
+			Assert.Throws<MemoryAllocationError>(throw_2);
+			Assert.Throws<MemoryAllocationError>(throw_3);
+			Assert.DoesNotThrow(throw_4);
+			VirtualMemory.Initialize(100000, 25);
+			VirtualMemory.Dump();
+			VirtualMemory.Destroy();
+		}
+
+		[Test ()]
+		public void VirtualMemoryTest ()
+		{
+			FeMoObject fmo = new FeMoObject ();
+			fmo.ID = 12;
+			fmo.Add ("test", "test");
+			fmo.Name = "Test";
+			VirtualMemory.Initialize (10000);
+			VirtualMemory.Push (fmo);
+			VirtualMemory.VarDump ();
+			FeMoObject fmo_ = new FeMoObject ();
+			fmo_.ID = 13;
+			fmo_.Name = "TestContainer";
+			VirtualMemory.Push (fmo_);
+			VirtualMemory.VarDump ();
+			fmo_.AddObject ("lol", fmo);
+			VirtualMemory.VarDump ();
+			VirtualMemory.Initialize(10000, 2);
+			VirtualMemory.Push(fmo);
+			VirtualMemory.WholeDump();
+			FeMoObject fmo_ = new FeMoObject();
+			fmo_.ID = 13;
+			fmo_.Name = "TestContainer";
+			VirtualMemory.Push(fmo_);
+			VirtualMemory.VarDump();
+			fmo_.AddObject("lol", fmo);
+			VirtualMemory.VarDump();
+			FeMoObject f_ = fmo_.GetObject("lol");
+			Console.WriteLine(f_.ToString());
+			Console.WriteLine(f_.GetString("test"));
+			VirtualMemory.Destroy();
+		}
+
+		[Test()]
+		public void MemorySpanTest ()
+		{
+			VirtualMemory.Initialize(10000, 5);
+			VirtualMemory.Dump();
+			FeMoObject fmo = new FeMoObject();
+			fmo.ID = VirtualMemory.GetFirstIdOf(VirtualMemory.GetMemorySpan('B'));
+			fmo.Name = "Test";
+			fmo.AddString("test", "test");
+			VirtualMemory.Push(fmo);
+			FeMoObject fmo_ = new FeMoObject();
+			fmo_.ID = VirtualMemory.GetFirstIdOf(VirtualMemory.GetMemorySpan('C'));
+			fmo_.Name = "ContainerTest";
+			fmo_.AddObject("lol", fmo);
+			VirtualMemory.Push(fmo_);
+			VirtualMemory.VarDump();
+			VirtualMemory.Destroy();
+		}
+
+		private void throw_1 ()
+		{
+			new MemorySpan (40, 30, 'A');
+		}
+
+		private void throw_2 ()
+		{
+			new MemorySpan (40, 40, 'B');
+		}
+
+		private void throw_3 ()
+		{
+			new MemorySpan (40, 50, 'C');
+		}
+
+		private void throw_4 ()
+		{
+			new MemorySpan (0, 10000, 'D');
+		}
+	}
+}
---
+++ SharedMemory/SharedMemoryBasic/VirtualMemory.cs
@@ -0,0 +1,207 @@
+using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Data;
+using System.Data.Linq;
+using System.Linq;
+using System.Xml.Linq; 
+
+namespace SharedMemory
+{
+	public class VirtualMemory : Dictionary<long, FeMoObject>
+	{
+		/*
+		 * Current Memory instance
+		 */
+		private static VirtualMemory instance;
+		/*
+		 * List of Memoryspans
+		 */
+		private MemorySpan[] memoryTable;
+
+		/*
+		 * The amount of reserved objects
+		 */
+		private long amount;
+		/*
+		 * Method to allocate the needed objects
+		 */
+		public static void Initialize (long amount, int parts)
+		{
+			instance = new VirtualMemory();
+			instance.amount = amount;
+			instance.allocMem(parts);
+		}
+		/*
+		 * Destroys the Virtual Memory
+		 */
+		public static void Destroy ()
+		{
+			instance = null;
+		}
+		/*
+		 * Returns a list containing the memory table
+		 */
+		public static List<MemorySpan> GetMemoryList ()
+		{
+			return instance.memoryTable.ToList<MemorySpan>();
+		}
+		/*
+		 * Returns the fitting Memory Span
+		 */
+		public static MemorySpan GetMemorySpan (char id)
+		{
+			var x = from m in instance.memoryTable where m.ID == id select m;
+			foreach (var y in x) {
+				return (MemorySpan) y;
+			}
+			throw new MemoryAddressException ("Invalid Key");
+		}
+		/*
+		 * Returns first free id of the given MemorySpan
+		 */
+		public static long GetFirstIdOf (MemorySpan span)
+		{
+			for (long i = span.Begin; i <= span.End; ++i) {
+				if(!InMemory(i))
+					return i;
+			}
+			throw new MemoryAddressException("Out of Range");
+		}
+		/*
+		 * Dumps the MemorySpan list to the Console
+		 */
+		public static void Dump ()
+		{
+			if(instance == null)
+				throw new MemoryNotInitializedException("Dump not possible, no memory!");
+			Console.WriteLine(instance.ToString());
+		}
+		/*
+		 * Dumps the whole memory to the Console
+		 */
+		public static void VarDump ()
+		{
+			Console.WriteLine("Used Space: " + instance.Keys.Count);
+			Console.WriteLine("Free Space: " + (instance.amount - instance.Keys.Count));
+			foreach(FeMoObject fmo in instance.Values) {
+				Console.WriteLine(fmo.ToString());
+			}
+		}
+
+		public static XElement GetXMLDump ()
+		{
+			if(instance == null)
+				throw new MemoryNotInitializedException("Dump not possible, no memory!");
+			return FeMoUtilities.XMLDump(instance.Values.ToList());
+		}
+
+		/*
+		 * Performs a WholeDump to the Console
+		 */
+		public static void WholeDump ()
+		{
+			Dump ();
+			VarDump();
+		}
+		/*
+		 * Checks if an object is currently in memory
+		 */
+		public static bool InMemory (long id)
+		{
+			try {
+				return instance[id] != null;
+			} catch (KeyNotFoundException) {
+				return false;
+			}
+		}
+		/*
+		 * Pushes an object into the Memory
+		 */
+		public static void Push (FeMoObject fmo)
+		{
+			if(fmo.ID > instance.amount)
+				throw new MemoryAddressException("Out of Memory Range");
+			if(InMemory(fmo.ID))
+				throw new MemoryAddressException("Memory already in use");
+			if(!InMemory(fmo.ID))
+				instance.Add(fmo.ID, fmo);
+		}
+		/*
+		 * Retrieves an object from the Memory
+		 */
+		public static FeMoObject Pull (long id)
+		{
+			if(!InMemory(id))
+				throw new MemoryWrongIdException("No Element at this id found");
+			if(InMemory(id))
+				return instance[id];
+			throw new Exception("Illegal Point of Operation");
+		}
+		/*
+		 * Creates the Memory Table
+		 */
+		private void allocMem(int parts) {
+			memoryTable = new MemorySpan[parts + 1];
+			long objs = (long)Math.Floor((double)((double)amount / (double)(parts + 1)));
+			for(int i = 0; i < parts; ++i) {
+				memoryTable[i] = new MemorySpan(objs * i, objs * (i + 1) - 1, (char)(i + 65));
+			}
+			memoryTable[parts] = new MemorySpan(objs * parts, amount, 'Z');
+		}
+
+		public override string ToString ()
+		{
+			String ret = "Memory Table:\n";
+			foreach (MemorySpan ms in memoryTable) {
+				ret += ms.ID + " : " + ms.Begin + " - " + ms.End + "\n";
+			}
+			return ret;
+		}
+	}
+
+	public class MemorySpan {
+		/*
+		 * The begin of the memory span
+		 */
+		private long begin;
+		/*
+		 * The end of the memory span
+		 */
+		private long end;
+		/*
+		 * The id of the memory span
+		 */
+		private char id;
+
+		public char ID {
+			get {
+				return id;
+			}
+		}
+
+		public long Begin {
+			get {
+				return begin;
+			}
+		}
+
+		public long End {
+			get {
+				return end;
+			}
+		}
+		public MemorySpan(long begin, long end, char id) {
+			if(begin > end)
+				throw new MemoryAllocationError("Illegal Memory Range (begin > end)");
+			if(begin == end)
+				throw new MemoryAllocationError("Illegal Memory Range (begin = end)");
+			if(end - begin <= 50)
+				throw new MemoryAllocationError("Illegal Memory Range (too few elements)");
+			this.begin = begin;
+			this.end = end;
+			this.id = id;
+		}
+	}
+}
+
