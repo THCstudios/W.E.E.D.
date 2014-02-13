fb5a2cbb72e20d7d8915c8fb0a17f7e726cdb58b
Momo <egg14829@spengergasse.at>
2014-02-13 14:39:12 +0100

    TypenAPI

---
+++ SharedMemory/TypenAPI.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SharedMemory {
+	class TypenAPI {
+		long id;
+
+		public long Id {
+			get { return id; }
+			set { id = value; }
+		}
+
+
+
+		FeMoManager manager;
+
+		public FeMoManager Manager {
+			get { return manager; }
+			set { manager = value; }
+		}
+
+		public TypenAPI(FeMoObject femo) {
+			if (femo.FetchEntry("EntityType") != null) {
+				Id = femo.Id;
+				Manager = femo.Manager;
+			} else {
+				throw new Exception("No EntityType");
+			}
+
+
+		}
+	}
+}
