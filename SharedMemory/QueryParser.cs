d9f0343f2c45bc9a3c07c0c647a7f27cedb43ef9
Momo <egg14829@spengergasse.at>
2014-04-08 12:38:03 +0200

    query

---
+++ SharedMemory/QueryParser.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SharedMemory {
+	class QueryParser {
+		public static FeMoObject Parse(String cmd) {
+			String[] split = cmd.Split(' ');
+			FeMoObject f = new FeMoObject();
+			if (split[0] == "GET") {
+				int id = Convert.ToInt32(split[1]);
+			} else if (split[0] == "SET") {
+
+			} else if (split[0] == "DEL") {
+				int id = Convert.ToInt32(split[1]);
+
+			} else if (split[0] == "INS") {
+
+			} else if (split[0] == "EXP") {
+
+			} else if (split[0] == "INP") {
+
+			} else if (split[0] == "DEF") {
+
+			} else if (split[0] == "ALT") {
+
+			}
+		}
+	}
+}
--- SharedMemory/TypenAPI.cs
+++ SharedMemory/TypenAPI.cs
@@ -14,7 +14,7 @@ namespace SharedMemory{
 			} catch (Exception e) {
 				value = femo.GetInt("EntityType");
 			}
-			
+
 			Id = femo.Id;
 			Manager = femo.Manager;
 			Convert.ToInt32(femo.GetInt("EntityType"));
@@ -25,12 +25,43 @@ namespace SharedMemory{
 
 			for (var i = 0; i <= table.Rows.Count - 1; i++) {
 				FeMoEntry entry = new FeMoEntry();
-				for (var j = 1; j <= table.Columns.Count-1; j++) {
+				for (var j = 1; j <= table.Columns.Count - 1; j++) {
 					var cell = table.Rows[i][j];
 					Console.WriteLine(cell);
 					if (j == 1) entry.Name = (String)cell;
 					else if (j == 2) entry.Value = (String)cell;
-					else if(j==3 && Global.CastEnum((String)cell) != Type.UNKNOWN)entry.Type = Global.CastEnum((String)cell);
+					else if (j == 3 && Global.CastEnum((String)cell) != Type.UNKNOWN) entry.Type = Global.CastEnum((String)cell);
+				}
+				base.AddEntry(entry);
+			}
+			Manager.SendUpdateString();
+			Global.DebugObject(this);
+		}
+
+		public TypenAPI(FeMoObject femo, String source) {
+			int value;
+			try {
+				value = base.GetInt("EntityType");
+			} catch (Exception e) {
+				value = femo.GetInt("EntityType");
+			}
+
+			Id = femo.Id;
+			Manager = femo.Manager;
+			Convert.ToInt32(femo.GetInt("EntityType"));
+			Console.WriteLine(value);
+			SQLiteBase db = new SQLiteBase(source);
+			DataTable table = db.ExecuteQuery("SELECT * FROM def where id = " + value + ";");
+			db.CloseDatabase();
+
+			for (var i = 0; i <= table.Rows.Count - 1; i++) {
+				FeMoEntry entry = new FeMoEntry();
+				for (var j = 1; j <= table.Columns.Count - 1; j++) {
+					var cell = table.Rows[i][j];
+					Console.WriteLine(cell);
+					if (j == 1) entry.Name = (String)cell;
+					else if (j == 2) entry.Value = (String)cell;
+					else if (j == 3 && Global.CastEnum((String)cell) != Type.UNKNOWN) entry.Type = Global.CastEnum((String)cell);
 				}
 				base.AddEntry(entry);
 			}
